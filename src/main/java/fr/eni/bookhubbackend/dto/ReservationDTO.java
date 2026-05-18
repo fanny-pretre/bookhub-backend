9c2dfa29b19067efa4c210cb812dd28c5bee79fc
Fanny PRETRE <fanny.pretre2025@campus-eni.fr>
2026-05-18 15:52:47 +0200

    feat: gestion reservation

---
+++ src/main/java/fr/eni/bookhubbackend/dto/ReservationDTO.java
@@ -0,0 +1,15 @@
+package fr.eni.bookhubbackend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationDTO {
+    @NotBlank
+    private String isbn;
+
+}
