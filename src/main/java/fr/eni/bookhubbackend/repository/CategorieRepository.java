034a9bdf12161ff6665e4a43b09cc070787df1d6
Agathe Perrin <agathe.perrin2023@campus-eni.fr>
2026-05-19 14:52:28 +0200

    fix: creation livre

--- src/main/java/fr/eni/bookhubbackend/repository/CategorieRepository.java
+++ src/main/java/fr/eni/bookhubbackend/repository/CategorieRepository.java
@@ -4,8 +4,10 @@ import fr.eni.bookhubbackend.entity.Categorie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategorieRepository extends JpaRepository<Categorie, Long> {
-
+    Optional<Categorie> findByTypeCategorie(String typeCategorie);
 }
 
